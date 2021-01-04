927d3725dce04a2bd847d18191db81fa545c8cd9
ParasKaushal123 <74295694+ParasKaushal123@users.noreply.github.com>
2021-01-04 20:16:05 +0530

    Create heloworld.py

---
+++ heloworld.py
@@ -0,0 +1,15 @@
+watches_Samuel_Miller = True
+pp_size = 0
+ifuckurmom = True
+
+def main():
+    if watches_Samuel_Miller == True:
+        pp_size = balls()
+    if pp_size == 69420:
+        print("Hello, World")
+    
+def balls():
+    # i fucked a dog
+    return 69420
+
+main()
